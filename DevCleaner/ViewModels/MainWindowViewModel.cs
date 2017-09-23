dcb80bf8203e9b78724fd6a7e1bb5836677e81de
Jorge Durán <ganchito55@gmail.com>
2017-09-24 01:10:14 +0200

    Add basic MVVM template

---
+++ DevCleaner/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Mvvm;
+
+namespace DevCleaner.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
