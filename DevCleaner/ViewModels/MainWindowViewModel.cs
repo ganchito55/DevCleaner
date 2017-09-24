a340c5fabe28d453faf19517242f78737db20d6d
Jorge Durán <ganchito55@gmail.com>
2017-09-24 21:23:03 +0200

    v0.9 Clean ui

--- DevCleaner/ViewModels/MainWindowViewModel.cs
+++ DevCleaner/ViewModels/MainWindowViewModel.cs
@@ -1,33 +1,72 @@
-﻿using System;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Windows.Threading;
 using DevCleaner.Models;
 using DevCleaner.Models.Projects;
 using Prism.Commands;
 using Prism.Mvvm;
+using Application = System.Windows.Application;
 
 namespace DevCleaner.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
+        private bool _cleanAllSolutions;
+
+        private int _cleanProgress;
+
+        private string _scanPath;
+
         public MainWindowViewModel()
         {
             ScanCommand = new DelegateCommand(Scan);
             CleanCommand = new DelegateCommand(Clean);
         }
 
-        private void Clean()
+        public string ScanPath
         {
-            foreach (var solution in Solutions)
+            get => _scanPath;
+            set => SetProperty(ref _scanPath, value);
+        }
+
+        public bool CleanAllSolutions
+        {
+            get => _cleanAllSolutions;
+            set
             {
-                solution.Projects.ForEach(t=>t.CleanProject());
+                SetProperty(ref _cleanAllSolutions, value);
+                foreach (var solution in Solutions)
+                    solution.IsSelected = true;
             }
         }
+
+        public int CleanProgress
+        {
+            get => _cleanProgress;
+            set => SetProperty(ref _cleanProgress, value);
+        }
+
+        public ObservableCollection<ISolution> Solutions { get; set; } = new ObservableCollection<ISolution>();
+
+        public DelegateCommand ScanCommand { get; }
+        public DelegateCommand CleanCommand { get; }
+
+        private void Clean()
+        {
+            new Task(() =>
+            {
+                foreach (var solution in Solutions)
+                {
+                    solution.CleanSolution();
+                    CleanProgress++;
+                }
+
+                CleanProgress = 0;
+            }).Start();
+        }
+
 
         private void Scan()
         {
@@ -45,10 +84,10 @@ namespace DevCleaner.ViewModels
         private void GetAllProjects()
         {
             var solutions = Directory.GetFiles(_scanPath, "*.sln", SearchOption.AllDirectories);
-            Regex projectRegex = new Regex(@"((\w| |\.)+\\)+(\w| |\.)+\.csproj");
+            var projectRegex = new Regex(@"((\w| |\.)+\\)+(\w| |\.)+\.csproj");
             foreach (var solution in solutions)
             {
-                ISolution solutionType = new DefaultSolution();
+                ISolution solutionType = new DefaultSolution(solution);
                 solutionType.Name = Path.GetFileNameWithoutExtension(solution);
 
                 var content = File.ReadAllText(solution);
@@ -56,46 +95,13 @@ namespace DevCleaner.ViewModels
                 foreach (Match project in projects)
                 {
                     //Get full path to the csproj file
-                    var projectType = Project.IdentifyProject(Path.Combine(Path.GetDirectoryName(solution),project.Value));
+                    var projectType =
+                        Project.IdentifyProject(Path.Combine(Path.GetDirectoryName(solution), project.Value));
                     if (projectType != null)
-                    {
                         solutionType.Projects.Add(projectType);
-                    }
                 }
-                App.Current.Dispatcher.Invoke(()=>
-                {
-                    Solutions.Add(solutionType);
-                });
-            }
-          
-        }
-
-        private string _scanPath;
-
-        public string ScanPath
-        {
-            get => _scanPath;
-            set => SetProperty(ref _scanPath, value);
-        }
-
-        private bool _cleanAllSolutions;
-
-        public bool CleanAllSolutions
-        {
-            get => _cleanAllSolutions;
-            set
-            {
-                SetProperty(ref _cleanAllSolutions, value);
-                foreach (var solution in Solutions)
-                {
-                    solution.IsSelected = true;
-                }
+                Application.Current.Dispatcher.Invoke(() => { Solutions.Add(solutionType); });
             }
         }
-
-        public ObservableCollection<ISolution> Solutions { get; set; } = new ObservableCollection<ISolution>();
-
-        public DelegateCommand ScanCommand { get; }
-        public DelegateCommand CleanCommand { get; }
     }
 }
